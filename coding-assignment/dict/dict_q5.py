23310dd753a0047ed8af2cb80da01e96d360f164
Tanya-123182 <tanyamakhija46@gmail.com>
2024-11-27 10:16:12 +0530

    key_value

---
+++ coding-assignment/dict/dict_q5.py
@@ -0,0 +1,7 @@
+#Write a Python program to iterate over a dictionary and print all key-value pairs.
+
+my_dict = {"name": "John", "age": 25}
+for key, value in my_dict.items():
+    print(key, ":", value)
+# name : John
+# age : 25
