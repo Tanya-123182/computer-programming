65a0bbe5ba4d5fada98cc1458419e58f830ab5e8
Tanya-123182 <tanyamakhija46@gmail.com>
2024-11-27 14:28:08 +0530

    find_duplicates

---
+++ coding-assignment/list/list_q14.py
@@ -0,0 +1,11 @@
+#Find All Duplicates in an Array
+
+def find_duplicates(arr):
+    seen = set()
+    duplicates = set()
+    for num in arr:
+        if num in seen:
+            duplicates.add(num)
+        else:
+            seen.add(num)
+    return list(duplicates)
