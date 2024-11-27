c8a300e071f4b82c28ae95402217bc1f2b9b7867
Tanya-123182 <tanyamakhija46@gmail.com>
2024-11-27 14:16:42 +0530

    paranthesis_check

---
+++ coding-assignment/list/list_q3.py
@@ -0,0 +1,16 @@
+def is_balanced(s):
+    stack = []
+    for char in s:
+        if char in "({[":
+            stack.append(char)
+        elif char in ")}]":
+            if not stack:
+                return False
+            top = stack.pop()
+            if char == ")" and top != "(":
+                return False
+            if char == "}" and top != "{":
+                return False
+            if char == "]" and top != "[":
+                return False
+    return len(stack) == 0
