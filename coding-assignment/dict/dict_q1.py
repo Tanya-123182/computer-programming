23a2cb2b54110b8b2e408233f99d5ca569593769
Tanya-123182 <tanyamakhija46@gmail.com>
2024-11-27 10:12:25 +0530

    assign_dict

---
+++ coding-assignment/dict/dict_q1.py
@@ -0,0 +1,5 @@
+#Write a Python program to add a new key-value pair to an existing dictionary.
+
+my_dict = {"name": "John", "age": 25}
+my_dict["address"] = "New York"
+print(my_dict)  # {'name': 'John', 'age': 25, 'address': 'New York'}
