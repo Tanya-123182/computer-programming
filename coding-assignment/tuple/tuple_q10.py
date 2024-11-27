b2991fea44db18ac0b70d95238c3e7289e915915
Tanya-123182 <tanyamakhija46@gmail.com>
2024-11-27 10:10:55 +0530

    common_keys

---
+++ coding-assignment/tuple/tuple_q10.py
@@ -0,0 +1,5 @@
+#Write a Python program to find the common keys between two dictionaries (use tuples for key-value pairs).
+
+dict1 = {('a', 1): 10, ('b', 2): 20}
+dict2 = {('a', 1): 30, ('c', 3): 40}
+common_keys = set(dict1.keys()) & set(dict2.keys
