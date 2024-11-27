482eae60d84875d158d71d00073eb4eeeaaeb36d
Tanya-123182 <tanyamakhija46@gmail.com>
2024-11-27 09:59:11 +0530

    largest_tuple

---
+++ coding-assignment/tuple/tuple_q2.py
@@ -0,0 +1,5 @@
+#Write a Python program to find the largest and smallest elements in a tuple.
+
+my_tuple = (10, 20, 4, 45, 99)
+print("Largest:", max(my_tuple))  # 99
+print("Smallest:", min(my_tuple))  # 4
